5e03a289fcff3ec7f4be7c7f46d8edb007c2f200
Rodrigo Galdino <63233782+rodrigogaldino553@users.noreply.github.com>
2020-10-22 23:48:35 -0200

    dados chegando na home

--- src/pages.js
+++ src/pages.js
@@ -12,10 +12,8 @@ module.exports = {
     async home(req, res) {
         const db = await database
         const users = await db.all(`SELECT name, user, photo FROM users`)
-        //console.log(users)
         console.log({users, test})
-        
-        return res.render('home.html', {users, test})
+        return res.render('home.html', { users, test })
     },
 
     async createUser(req, res) {
@@ -26,42 +24,50 @@ module.exports = {
         }
         try {
             const db = await database
-            await newUser(db, { name: data.name, user: data.user, password: data.password, photo: data.photo })
+
+            const verify = await db.all(`SELECT * FROM users WHERE user="${data.user}"`)
+            
+
+            if (verify.length > 0) {
+                res.send(`ERRO! ${data.user} já existe!`)
+
+            } else {
+                await newUser(db, { name: data.name, user: data.user, password: data.password, photo: data.photo })
+                return res.redirect('/')
+                
+            }
         } catch (error) {
             console.log(error)
+            res.send('ERRO AO SALVAR USUARIO')
         }
 
-        res.redirect('/home')
+
         /*const obj = req.body
         res.send(`<h1>${data.name}</h1><img src="${data.photo}" alt="Padrao">`)*/
     },
 
     async login(req, res) {
         const data = req.body
-        //var login = []
-        
+
         try {
             const db = await database
             const login = await db.all(`SELECT * FROM users WHERE user="${data.user}" AND password="${data.password}";`)//user="${data.name}" FROM users WHERE password="${data.password}";`)
-            
-            if(login.length > 0){
+
+            if (login.length > 0) {
                 test.name = login[0].name
                 test.user = login[0].user
                 test.photo = login[0].photo
 
-                
+
                 return res.redirect('/home')
-            }else{
+            } else {
                 return res.send('DADOS NÃO CONFEREM!')
             }
         } catch (error) {
-            console.log(error) 
+            console.log(error)
             return res.send('FALHA AO CONSULTAR DADOS!')
         }
-        
-        
 
-        
 
     }
 }
